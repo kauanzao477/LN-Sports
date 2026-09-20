63bf6b030eede028e1a5fe5fbb1d57e4e0183af1
Kauan <kauanhenriquesilvabalestrin@gmail.com>
2026-09-19 23:28:21 -0300

    fix: corrige imagem de capa dos tenis e listagem de produtos do admin5

---
+++ loja/.wrangler/tmp/pages-kCUw73/functionsRoutes-0.7819248325637205.mjs
@@ -0,0 +1,227 @@
+import { onRequestOptions as __api_admin_products__id__cover_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id]\\cover.js"
+import { onRequestPatch as __api_admin_products__id__cover_js_onRequestPatch } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id]\\cover.js"
+import { onRequestOptions as __api_admin_products__id__featured_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id]\\featured.js"
+import { onRequestPatch as __api_admin_products__id__featured_js_onRequestPatch } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id]\\featured.js"
+import { onRequestOptions as __api_admin_products__id__main_image_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id]\\main-image.js"
+import { onRequestPatch as __api_admin_products__id__main_image_js_onRequestPatch } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id]\\main-image.js"
+import { onRequestOptions as __api_admin_products__id__publish_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id]\\publish.js"
+import { onRequestPatch as __api_admin_products__id__publish_js_onRequestPatch } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id]\\publish.js"
+import { onRequestDelete as __api_admin_products__id__js_onRequestDelete } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id].js"
+import { onRequestOptions as __api_admin_products__id__js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id].js"
+import { onRequestPut as __api_admin_products__id__js_onRequestPut } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\products\\[id].js"
+import { onRequestOptions as __api_admin_categories_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\categories.js"
+import { onRequestPut as __api_admin_categories_js_onRequestPut } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\categories.js"
+import { onRequestGet as __api_admin_dashboard_js_onRequestGet } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\dashboard.js"
+import { onRequestOptions as __api_admin_dashboard_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\dashboard.js"
+import { onRequestOptions as __api_admin_login_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\login.js"
+import { onRequestPost as __api_admin_login_js_onRequestPost } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\login.js"
+import { onRequestOptions as __api_admin_settings_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\settings.js"
+import { onRequestPut as __api_admin_settings_js_onRequestPut } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\admin\\settings.js"
+import { onRequestGet as __api_products__slug__js_onRequestGet } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\products\\[slug].js"
+import { onRequestOptions as __api_products__slug__js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\products\\[slug].js"
+import { onRequestGet as __api_categories_js_onRequestGet } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\categories.js"
+import { onRequestOptions as __api_categories_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\categories.js"
+import { onRequestGet as __api_products_index_js_onRequestGet } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\products\\index.js"
+import { onRequestOptions as __api_products_index_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\products\\index.js"
+import { onRequestGet as __api_settings_js_onRequestGet } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\settings.js"
+import { onRequestOptions as __api_settings_js_onRequestOptions } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\settings.js"
+import { onRequest as __api_image_proxy_js_onRequest } from "C:\\Users\\kauan\\Downloads\\LN-Sports-main (2)\\LN-Sports-main\\LN-Sports-main\\loja\\functions\\api\\image-proxy.js"
+
+export const routes = [
+    {
+      routePath: "/api/admin/products/:id/cover",
+      mountPath: "/api/admin/products/:id",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_admin_products__id__cover_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/admin/products/:id/cover",
+      mountPath: "/api/admin/products/:id",
+      method: "PATCH",
+      middlewares: [],
+      modules: [__api_admin_products__id__cover_js_onRequestPatch],
+    },
+  {
+      routePath: "/api/admin/products/:id/featured",
+      mountPath: "/api/admin/products/:id",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_admin_products__id__featured_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/admin/products/:id/featured",
+      mountPath: "/api/admin/products/:id",
+      method: "PATCH",
+      middlewares: [],
+      modules: [__api_admin_products__id__featured_js_onRequestPatch],
+    },
+  {
+      routePath: "/api/admin/products/:id/main-image",
+      mountPath: "/api/admin/products/:id",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_admin_products__id__main_image_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/admin/products/:id/main-image",
+      mountPath: "/api/admin/products/:id",
+      method: "PATCH",
+      middlewares: [],
+      modules: [__api_admin_products__id__main_image_js_onRequestPatch],
+    },
+  {
+      routePath: "/api/admin/products/:id/publish",
+      mountPath: "/api/admin/products/:id",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_admin_products__id__publish_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/admin/products/:id/publish",
+      mountPath: "/api/admin/products/:id",
+      method: "PATCH",
+      middlewares: [],
+      modules: [__api_admin_products__id__publish_js_onRequestPatch],
+    },
+  {
+      routePath: "/api/admin/products/:id",
+      mountPath: "/api/admin/products",
+      method: "DELETE",
+      middlewares: [],
+      modules: [__api_admin_products__id__js_onRequestDelete],
+    },
+  {
+      routePath: "/api/admin/products/:id",
+      mountPath: "/api/admin/products",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_admin_products__id__js_onRequestOptions],
+    },
+  {
+      routePath: "/api/admin/products/:id",
+      mountPath: "/api/admin/products",
+      method: "PUT",
+      middlewares: [],
+      modules: [__api_admin_products__id__js_onRequestPut],
+    },
+  {
+      routePath: "/api/admin/categories",
+      mountPath: "/api/admin",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_admin_categories_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/admin/categories",
+      mountPath: "/api/admin",
+      method: "PUT",
+      middlewares: [],
+      modules: [__api_admin_categories_js_onRequestPut],
+    },
+  {
+      routePath: "/api/admin/dashboard",
+      mountPath: "/api/admin",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_admin_dashboard_js_onRequestGet],
+    },
+  {
+      routePath: "/api/admin/dashboard",
+      mountPath: "/api/admin",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_admin_dashboard_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/admin/login",
+      mountPath: "/api/admin",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_admin_login_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/admin/login",
+      mountPath: "/api/admin",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_admin_login_js_onRequestPost],
+    },
+  {
+      routePath: "/api/admin/settings",
+      mountPath: "/api/admin",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_admin_settings_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/admin/settings",
+      mountPath: "/api/admin",
+      method: "PUT",
+      middlewares: [],
+      modules: [__api_admin_settings_js_onRequestPut],
+    },
+  {
+      routePath: "/api/products/:slug",
+      mountPath: "/api/products",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_products__slug__js_onRequestGet],
+    },
+  {
+      routePath: "/api/products/:slug",
+      mountPath: "/api/products",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_products__slug__js_onRequestOptions],
+    },
+  {
+      routePath: "/api/categories",
+      mountPath: "/api",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_categories_js_onRequestGet],
+    },
+  {
+      routePath: "/api/categories",
+      mountPath: "/api",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_categories_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/products",
+      mountPath: "/api/products",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_products_index_js_onRequestGet],
+    },
+  {
+      routePath: "/api/products",
+      mountPath: "/api/products",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_products_index_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/settings",
+      mountPath: "/api",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_settings_js_onRequestGet],
+    },
+  {
+      routePath: "/api/settings",
+      mountPath: "/api",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_settings_js_onRequestOptions],
+    },
+  {
+      routePath: "/api/image-proxy",
+      mountPath: "/api",
+      method: "",
+      middlewares: [],
+      modules: [__api_image_proxy_js_onRequest],
+    },
+  ]
